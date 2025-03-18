4aabca34a3c21a3fd1ca4003cce68e0d9e1c1571
maik5h <maik.hylinski@student.uni-tuebingen.de>
2025-03-18 19:58:46 +0100

    Forgot to add GUI header.

---
+++ src/GUI.h
@@ -0,0 +1,25 @@
+// Functions to manage GUI, definitions are platform dependend.
+// So far, only windows is implemented.
+
+#pragma once
+
+#include "UDShaper.h"
+#include <windowsx.h>
+#include "assets.h"
+#include "../clap/clap.h"
+
+struct GUI {
+	HWND window; // GUI window.
+	uint32_t *bits; // GUI bitmap buffer. Has size GUI_WIDTH * GUI_HEIGHT with eight bit for each color channel and alpha channel.
+};
+
+void GUIPaint(UDShaper *plugin, bool internal);
+void GUICreate(UDShaper *plugin, clap_plugin_descriptor_t pluginDescriptor);
+void GUIDestroy(UDShaper *plugin, clap_plugin_descriptor_t pluginDescriptor);
+void GUIOnPOSIXFD(UDShaper *plugin);
+
+#ifdef _WIN32
+#define GUISetParent(plugin, parent) SetParent((plugin)->gui->window, (HWND) (parent)->win32)
+#define GUISetVisible(plugin, visible) ShowWindow((plugin)->gui->window, (visible) ? SW_SHOW : SW_HIDE)
+#define GUI_API CLAP_WINDOW_API_WIN32
+#endif
